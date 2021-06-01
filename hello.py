fcc13b925c7047aa6447e1784bf090e1f599082d
itzzmesid <sidharths550@gmail.com>
2021-06-01 12:42:05 +0530

    Adding HTML pages

--- hello.py
+++ hello.py
@@ -1,5 +1,12 @@
+"""
+How to run the file?
+$ export FLASK_APP=hello
+$ flask run
+"""
+
 from flask import Flask, redirect, url_for
 from markupsafe import escape 	
+
 app = Flask(__name__)
 
 @app.route("/")
@@ -20,19 +27,13 @@ def user(name):
 def rd():
 	return redirect(url_for("hello_world"))
 # Here whenever we goes to /admin , 
-# we will get redirected to hello_world function and the
-# func displays the contents in it.
+# we will get redirected to hello_world function and the func displays the contents in it.
 
+# Now imagine we need to redirect to user() and print `Hello Admin`. So we have to give `return redirect(url_for("user"))`.
+# But how will we pass the name as `Admin`? For that , we will explicitly pass the value to the function param like below:
+# return redirect(url_for("user",name="Admin"))
 
-
-
-
-
-
-
-
-"""
-How to run the file?
-$ export FLASK_APP=hello
-$ flask run
-"""
+@app.route('/post/<int:post_id>') # -> http://127.0.0.1:5000/post/1
+def show_post(post_id):
+    # show the post with the given id, the id is an integer
+    return f'Post {post_id}'
--- workingwithhtml.py
+++ workingwithhtml.py
@@ -1,6 +1,16 @@
-from flask import Flask
+# Maintain a folder named  `templates` for storing the html pages
+
+from flask import Flask, render_template
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-	return "<h1>Hello World</h1>"
+@app.route("/child1")
+def inherited():
+	return render_template("child1.html")
+
+@app.route("/<user>")
+def home(user):
+	return render_template("index.html",content=user, r=2) # We can pass multiple values here
+	# We can pass even a list as values
+	# Eg - return render_template("index.html",value=['s','d','f']) . In corresponding HTML page we can access this list via 
+	# mentioning as {{value}} within a for loop to traverse all the elements inside the list
+
